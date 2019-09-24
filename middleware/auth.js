6db44e9f1a2ec344cbea9f2bd08b6b2ed4f53a69
Daniel Byun <dbyun@doinglab.com>
2019-09-24 18:50:21 +0900

    Custom Auth Middleware - JWT Verify

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+  // Get token from the header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if (!token) {
+    // not authorized
+    return res.status(401).json({ msg: "No token, authorization denied." });
+  }
+
+  // Verify the token if there is one
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ msg: "Token is not valid." });
+  }
+};
