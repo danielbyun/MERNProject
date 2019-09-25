3f738af354b64d93417bfd4f17c4a2216f092300
Daniel Byun <dbyun@doinglab.com>
2019-09-25 20:04:54 +0900

    Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts route"));
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) {
+      return res.status(400).send({ errors: error.array() });
+    }
+
+    try {
+      // already authenticated so can access the id via req.user.id
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
