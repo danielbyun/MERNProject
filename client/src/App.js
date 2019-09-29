941f2014fda95c2bc0f374444869da090530e1e1
Daniel Byun <dbyun@doinglab.com>
2019-09-29 11:45:24 +0900

    Protected Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import { Provider } from "react-redux";
 import store from "./Store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,6 +36,8 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               />
               <Route exact path="/login" component={Login} />
+              />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               />
             </Switch>
           </section>
